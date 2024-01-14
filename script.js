f13fe26d4ec2e14cd3886d79ebf69e67cfb9c032
Konucho <91157233+Konucho@users.noreply.github.com>
2024-01-14 19:39:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,38 @@ searchBtn.addEventListener("click", () => {
     search.style.backgroundColor = "white";
     search.style.filter = "drop-shadow(0px 0.5px 0.5px grey)";
     search.focus();
+
+    search.addEventListener("mouseenter", () => {
+        search.style.color = "black";
+        search.style.width = "600px";
+    });
+
+    search.addEventListener("mouseleave", () => {
+        search.style.color = "white";
+        search.style.paddingLeft = "60px";
+        search.style.width = "20px";
+    });
+    
+    search.addEventListener("focusout", () => {
+        search.style.paddingLeft = "60px";
+        search.style.width = "20px";
+    });
+
+    searchBtn.addEventListener("mouseenter", () => {
+        search.style.color = "black";
+        search.style.width = "600px";
+    });
+
+    searchBtn.addEventListener("mouseleave", () => {
+        search.style.color = "white";
+        search.style.paddingLeft = "60px";
+        search.style.width = "20px";
+    });
+    
+    searchBtn.addEventListener("focusout", () => {
+        search.style.paddingLeft = "60px";
+        search.style.width = "20px";
+    });
+
 })
 
