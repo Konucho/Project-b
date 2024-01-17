1d9479b330d4c108508cd10f95c8b76697c8a15f
Konucho <91157233+Konucho@users.noreply.github.com>
2024-01-17 13:00:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -74,3 +74,16 @@ productThree.addEventListener("mouseout", () => {
     productThree.textContent = originalTextThree;
 }); 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
